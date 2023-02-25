ff62a1ce0f3227d647be8989b6396efc11f6d69d
KevinZ0229 <87968349+KevinZ0229@users.noreply.github.com>
2023-02-25 13:58:49 -0500

    Initial Commit

---
+++ Count/CountApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountApp.swift
+//  Count
+//
+//  Created by Hengchuan Zhang on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct CountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
